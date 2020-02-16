3a95746984235de61f8be5f532dd2f15aee474f6
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-02-16 12:43:28 -0300

    Usar e manipular o state - adicionar valores.

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: [
       'NodeJS',
       'ReactJS',
@@ -9,14 +10,29 @@ class TechList extends Component {
     ]
   };
 
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '' });
+  }
+
   render() {
-    console.log(this.state);
     return (
-      <ul>
-        <li>NodeJS</li>
-        <li>ReactJS</li>
-        <li>ReactNative</li>
-      </ul> 
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTech}</h1>
+        <ul>{this.state.techs.map(tech => <li key={tech}>{tech}</li>)}</ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     )
   }
 }
