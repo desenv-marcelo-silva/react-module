8fd33e5c04b85b78889f706a60b7a2b93623d5d4
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-02-16 12:09:22 -0300

    Instalação, configuração e exemplo de uso do file-loader.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import profile from './assets/img/foto-microsoft.jpg';
 
 function App() {
-  return <h1>Hello React with Rocketseat</h1>
+  return <img src={profile} width="500"/>
 }
 
 export default App;
